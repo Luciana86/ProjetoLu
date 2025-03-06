acf0f05ab6a532c82ead3a650012f7d02932470a
Luciana86 <lucianapuc@gmail.com>
2025-03-06 11:09:05 -0300

    Alteracao

--- src/test/java/apiModules/APITestsRefactor.java
+++ src/test/java/apiModules/APITestsRefactor.java
@@ -50,6 +50,7 @@ public class APITestsRefactor {
                 get("/unknown").
                 then().
                 statusCode(200).
+                body("data[1].name",equalTo("fuchsia rose")).
                 body("data[2].name",equalTo("true red")).
                 body("data[3].name",equalTo("aqua sky"));
 
@@ -64,15 +65,18 @@ public class APITestsRefactor {
                 .get("/unknown");
 
         // Imprime apenas uma parte específica da resposta
+        String nomeSegundo = response.jsonPath().getString("data[1].name");
         String nomeTerceiro = response.jsonPath().getString("data[2].name");
         String nomeQuarto = response.jsonPath().getString("data[3].name");
 
+        System.out.println("Nome do segundo item: " + nomeSegundo);
         System.out.println("Nome do terceiro item: " + nomeTerceiro);
         System.out.println("Nome do quarto item: " + nomeQuarto);
 
         // Valida o status e os nomes
         response.then()
                 .statusCode(200)
+                .body("data[1].name", equalTo("fuchsia rose"))
                 .body("data[2].name", equalTo("true red"))
                 .body("data[3].name", equalTo("aqua sky"));
 
